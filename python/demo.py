9368fbc488e3eb793002bf8f168f9653b7e6ad75
NickLi <nickli@nicklidembp.lan>
2019-10-09 23:11:16 -0700

    add html

--- python/demo.py
+++ python/demo.py
@@ -1,6 +1,28 @@
 import numpy as np
-import http.server
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import SimpleHTTPServer
+import BaseHTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+# from SimpleHTTPServer import HTTPServer
+
+class S(BaseHTTPRequestHandler):
+
+    def __init__(self, request, client_address, server):
+        BaseHTTPRequestHandler.__init__(self, request, client_address, server)
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # prepare for response
+        self._set_headers()
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     server_address = ('', port)
