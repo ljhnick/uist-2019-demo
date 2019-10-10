44bf8bb500cf83e7f0f09879cc6a15b3829c31ba
NickLi <nickli@nicklidembp.lan>
2019-10-09 23:00:26 -0700

    initial commit

---
+++ python/demo.py
@@ -0,0 +1,17 @@
+import numpy as np
+import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
